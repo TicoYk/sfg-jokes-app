5a166d8e00b204e2fc4f8e17dedda8f4883f6bab
TicoYk <didimoykym@gmail.com>
2020-12-26 13:07:07 -0300

    Implemented ChuckNorris Quote

--- src/main/java/com/ticoyk/sfgjokesapp/service/NorrisJokeService.java
+++ src/main/java/com/ticoyk/sfgjokesapp/service/NorrisJokeService.java
@@ -4,16 +4,21 @@ import com.ticoyk.sfgjokesapp.model.Joke;
 
 import org.springframework.stereotype.Service;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
 @Service("norrisJokeService")
 public class NorrisJokeService implements JokeService {
 
     private Joke joke;
+    private ChuckNorrisQuotes chuckNorrisQuotes;
 
     public NorrisJokeService(Joke joke){
         this.joke = joke;
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
     public Joke getJoke(){
+        this.joke.setText(chuckNorrisQuotes.getRandomQuote());
         return joke;
     }
     
